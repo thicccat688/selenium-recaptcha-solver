40f86f2bd358f26e7475eb4b2e9842f8dd9c4972
thicccat688 <tomasperestrelo21@gmail.com>
2022-12-23 14:56:21 +0000

    Change tests to run headlessly.

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,7 @@ import pytest
 
 options = webdriver.ChromeOptions()
 
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 
 options.add_argument('--no-sandbox')
