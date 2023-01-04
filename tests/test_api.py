1aeb8827397598d10bb70aaf121f8da74d416389
thicccat688 <tomasperestrelo21@gmail.com>
2023-01-04 13:03:57 +0000

    Test case improvements, project files refactoring.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,24 @@
 from selenium.webdriver.support import expected_conditions as ec
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.service import Service
 from selenium_recaptcha_solver.api import API
 import undetected_chromedriver as webdriver
 import pytest
 
+
+test_ua = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
 
 options = webdriver.ChromeOptions()
 
 options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 
+options.add_argument(f'--user-agent={test_ua}')
 options.add_argument('--incognito')
-options.add_argument('--no-sandbox')
 
+options.add_argument('--no-sandbox')
 options.add_argument("--disable-extensions")
 
-service = Service(ChromeDriverManager().install())
-
-test_driver = webdriver.Chrome(options=options, service=service)
+test_driver = webdriver.Chrome(options=options)
 
 solver = API(driver=test_driver)
 
