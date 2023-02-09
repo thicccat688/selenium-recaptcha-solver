8a5cced0371366dbbb49f8d8fd1c64a4752d15c7
Alexander Regueiro <alexreg@gmail.com>
2023-02-09 18:30:47 +0000

    Add docs for services

--- selenium_recaptcha_solver/services.py
+++ selenium_recaptcha_solver/services.py
@@ -5,16 +5,24 @@ import speech_recognition as sr
 
 
 class Service(ABC):
+    """Abstract base class for speech recognition services."""
+
     @abstractmethod
     def __init__(self) -> None:
         pass
 
     @abstractmethod
     def recognize(self, recognizer: sr.Recognizer, audio: AudioData) -> Any:
+        """Perform speech recognition on the given audio data using the given recognizer."""
         pass
 
 
 class BingService(Service):
+    """Service for Microsoft Bing Speech API.
+
+    See docs for `speech_recognition.Recognizer.recognize_bing` for more information on credentials.
+    """
+
     def __init__(
         self,
         key: Any,
@@ -26,6 +34,11 @@ class BingService(Service):
 
 
 class GoogleService(Service):
+    """Service for Google Speech Recognition API.
+
+    See docs for `speech_recognition.Recognizer.recognize_google` for more information on credentials.
+    """
+
     def __init__(
         self,
         key: Optional[Any] = None,
@@ -37,6 +50,11 @@ class GoogleService(Service):
 
 
 class GoogleCloudService(Service):
+    """Service for Google Cloud Speech API.
+
+    See docs for `speech_recognition.Recognizer.recognize_google_cloud` for more information on credentials.
+    """
+
     def __init__(
         self,
         credentials_json: Optional[Any] = None,
@@ -44,10 +62,17 @@ class GoogleCloudService(Service):
         self.credentials_json = credentials_json
 
     def recognize(self, recognizer: sr.Recognizer, audio: AudioData) -> Any:
-        return recognizer.recognize_google_cloud(audio, credentials_json=self.credentials_json)
+        return recognizer.recognize_google_cloud(
+            audio, credentials_json=self.credentials_json
+        )
 
 
 class HoundifyService(Service):
+    """Service for Houndify API.
+
+    See docs for `speech_recognition.Recognizer.recognize_houndify` for more information on credentials.
+    """
+
     def __init__(
         self,
         client_id: Any,
@@ -57,10 +82,17 @@ class HoundifyService(Service):
         self.client_key = client_key
 
     def recognize(self, recognizer: sr.Recognizer, audio: AudioData) -> Any:
-        return recognizer.recognize_houndify(audio, client_id=self.client_id, client_key=self.client_key)
+        return recognizer.recognize_houndify(
+            audio, client_id=self.client_id, client_key=self.client_key
+        )
 
 
 class IbmService(Service):
+    """Service for IBM Speech to Text API.
+
+    See docs for `speech_recognition.Recognizer.recognize_google_cloud` for more information on credentials.
+    """
+
     def __init__(
         self,
         key: Any,
@@ -72,6 +104,11 @@ class IbmService(Service):
 
 
 class SphinxService(Service):
+    """Service for CMU Sphinx.
+
+    See docs for `speech_recognition.Recognizer.recognize_sphinx` for more information on credentials.
+    """
+
     def __init__(
         self,
         grammar: Any = None,
@@ -83,6 +120,11 @@ class SphinxService(Service):
 
 
 class WitService(Service):
+    """Service for Wit.ai API.
+
+    See docs for `speech_recognition.Recognizer.recognize_wit` for more information on credentials.
+    """
+
     def __init__(
         self,
         key: Any,
