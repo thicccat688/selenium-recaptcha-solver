96c158e55885aab4b9f25c49969b0b452f558ad6
thicccat688 <tomasperestrelo21@gmail.com>
2023-02-21 21:45:48 +0000

    Include API in __init__.py. (Credit to Aykhan-s)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selenium-recaptcha-solver',
-    version='1.2.0',
+    version='1.2.1',
     license='MIT',
     author='Tomás Perestrelo',
     author_email='tomasperestrelo21@gmail.com',
