274f13b9c683d5c0aebe989304b8c25445dc7089
Aykhan-s <aykhan.shahs0@gmail.com>
2023-02-22 01:30:50 +0400

    API object added to __init__.py

--- selenium_recaptcha_solver/__init__.py
+++ selenium_recaptcha_solver/__init__.py
@@ -1,4 +1,4 @@
 from . import services
 from .exceptions import RecaptchaException
 from .delay_config import DelayConfig, StandardDelayConfig
-from .solver import RecaptchaSolver
+from .solver import RecaptchaSolver, API
