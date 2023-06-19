eed531ea857725ad1a4a9308c9ab346a11f07fa7
thicccat688 <tomasperestrelo21@gmail.com>
2023-06-19 12:27:44 +0100

    Change requirements.txt to use SpeechRecognition v3.8.1 to avoid printing out unwanted confidence scores.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='selenium-recaptcha-solver',
-    version='1.8.1',
+    version='1.8.3',
     license='MIT',
     author='Tomás Perestrelo',
     author_email='tomasperestrelo21@gmail.com',
